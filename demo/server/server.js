88dcc669af5a494480ab36a662cd2fd37c72ad3a
Travis Woolston <travis.ww@hotmail.com>
2021-08-14 15:17:40 -0600

    saturday graphiql

--- demo/server/server.js
+++ demo/server/server.js
@@ -3,116 +3,26 @@ const dotenv = require("dotenv").config();
 const path = require("path");
 const app = express();
 const gqlHTTP = require("express-graphql");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1500;
 const schema = require('./schema/schema');
-
-const { ApolloServer } = require('apollo-server-express')
-const { RESTDataSource } = require('apollo-datasource-rest')
-// const typeDefs = require('./schema')
-// const resolvers = require('./resolvers')
-const { ApolloServerPluginLandingPageGraphQLPlayground,
-ApolloServerPluginLandingPageProductionDefault,
-ApolloServerPluginLandingPageLocalDefault
- } = require('apollo-server-core');
+const RediQLCache = require('./RediQLCache/src/RediQL')
 
 
-const gql = require('graphql-tag')
+const redis = require('redis')
+const REDIS_PORT = process.env.PORT || 6379
 
-const typeDefs = gql`
-  type Launch {
-    flight_number: Int!
-    mission_name: String!
-    launch_date_utc: String!
-    launch_success: Boolean!
-    cost_per_launch: Int!
-    rocket: Rocket!
-  }
+const client = redis.createClient(REDIS_PORT)
 
-  type Rocket {
-    rocket_id: String!
-    rocket_name: String!
-    rocket_type: String!
-    cost_per_launch: Int!
-    boosters: Int!
-  }
-
-  type Query {
-    getLaunches: [Launch!]
-    
-  }
-`
-
-const resolvers = {
-  Query: {
-    getLaunches: async (_, { flight_number }, { dataSources } ) =>{
-      return dataSources.SpaceXAPI.getLaunch(flight_number)
-    }
-  }
-}
-
-// Movies API Data Source
-class SpaceXAPI extends RESTDataSource{
-  constructor() {
-    super();
-    this.baseURL = 'https://api.spacexdata.com/v3/'
-
-  }
-  async getLaunch(flight_number) {
-    return this.getLaunch(`launches/${flight_number}`)
-  }
-  async getRocket(id) {
-    return this.getLaunch(`rockets/${id}`)
-  }
-}
-
-
-// create apollo server constructor
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  graphiql: true,
-  dataSources: () => {
-    return {
-      spaceXAPI: new SpaceXAPI()
-    }
-  },
-  plugins: [
-   {
-     async serverWillStart(){
-       return {
-         async renderLandingPage(){
-           return ApolloServerPluginLandingPageLocalDefault({ footer: false }) 
-         }
-       }
-     }
-   }
-  ]
+client.on("error", (err) => {
+  console.log(err)
 })
-async function startApolloServer(){
-  
-  await server.start();
 
-  const app = express();
-  // route for additional middlware
-  // server.applyMiddleware({ app });
-  // app.use('/', )
-  const PORT = process.env.PORT || 5000
-;
-  await new Promise(resolve => app.listen({ port: PORT }, resolve));
-  console.log(`Server ready at ${PORT}`);
-  return { server, app }
-}
+//redis caching will be done within this object
+// const rediQlCache = new RediQLCache(schema, redixPort, 3600)
 
-startApolloServer()
-// const redis = require('redis')
-// const REDIS_PORT = process.env.PORT || 6379
+app.use(express.json());
 
-// const client = redis.createClient(REDIS_PORT)
-
-// app.use(express.json());
-
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // /**
 //  * @description
@@ -124,7 +34,7 @@ startApolloServer()
 //  *
 //  */
 
-// app.use("/graphql", gqlHTTP.graphqlHTTP({ schema, graphiql: true }));
+app.use("/graphql", gqlHTTP.graphqlHTTP({ schema, graphiql: true }));
 
 // // Default Error Handler
 // app.use((err, req, res, next) => {
@@ -143,6 +53,6 @@ startApolloServer()
 // app.use((req, res) => res.status(404).send("Page not found!"));
 
 //start server
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port: ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}/graphql`);
+});
