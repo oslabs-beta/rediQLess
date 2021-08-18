49c9d6a5c676ffed546a5953c6afae298e59b6f3
Eric Saldivar <Eric@samlas-mbp.lan>
2021-08-17 21:13:48 -0500

    added JSDOC to author container page

--- demo/client/containers/authorscontainers.tsx
+++ demo/client/containers/authorscontainers.tsx
@@ -1,7 +1,15 @@
+/**
+ * This is a JSX container that imports AuthorProfile JSX component and iterates over the authorData
+ * to generate the author information for the page
+ * @param {string} name is the name of the author
+ * @param {string} image is the url of the image
+ * @param {string} info is some information about the author
+ * @param {string} github is the github url of the author
+ * @param {string} linkedin is the linkedin url of the author
+ */
 import React from 'react';
 import AuthorProfile from '../components/authors';
 import authorData from '../util/authorinfo';
-
 
 const AuthorsContainer = () => {
 
@@ -10,9 +18,7 @@ const AuthorsContainer = () => {
 			{authorData.map((el, index:number) => {
 			return <AuthorProfile key={index} name={el.name} image={el.image} info={el.info} github={el.github} linkedin={el.linkedin}/>
 		})}	
-		</div>
-		
-		
+		</div>		
 	)
 }
 
