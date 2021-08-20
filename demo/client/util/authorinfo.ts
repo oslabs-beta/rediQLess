e01d04b42f36fc12ebc40610b0742dadd68ab7a5
Eric Saldivar <Eric@samlas-mbp.lan>
2021-08-19 21:04:00 -0500

    lorem place holder for author info

--- demo/client/util/authorinfo.ts
+++ demo/client/util/authorinfo.ts
@@ -2,29 +2,28 @@ const authorData = [
 	{
 		name: "Charlie Malave",
 		image: "https://rediqless.s3.us-east-2.amazonaws.com/1629337049016.jpeg",
-		info: "22.76 USD",
+		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
 		github: "https://github.com/cmalave13",
 		linkedin: 'https://www.linkedin.com/in/charlesmalave/'
 	  },
 	  {
 		name: "Eric Saldivar",
 		image: "https://rediqless.s3.us-east-2.amazonaws.com/profile.jpg",
-		info: `Me in nutshell? I am a veteran of the Air Force, a former elementary school teacher, a father of boy/girl twins, and a husband. I love to travel and I have been to 25 countries and 5 continents. I love to watch movies, I am a sucker for Marvel movies, I love to find new places to eat, and I love to find new science fiction/ fantasy fiction whether it is movies or literature.
-		Specifically about software engineering, I love to learn new technologies, frameworks, and libraries. I enjoy tackling projects for work and for fun. I have worked in and lead an agile/SCRUM model of production. I really like to touch all areas of the life cycle of planning, designing, implementing, testing, deploying, and maintaining a project. I hope you have gained some insight into me as a person and a software engineer.`,
+		info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
 		github: "https://github.com/esaldivar",
 		linkedin: 'https://www.linkedin.com/in/esaldivar1214/'
 	  },
 	  {
 		name: "Ian Judd",
 		image: "https://rediqless.s3.us-east-2.amazonaws.com/Screen+Shot+2021-08-19+at+8.34.34+PM.png",
-		info: "341.79 USD",
+		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
 		github: "https://github.com/ikjudd",
 		linkedin: 'https://www.linkedin.com/in/iankjudd/'
 	  },
 	  {
 		name: "Travis Woolston",
 		image: "https://rediqless.s3.us-east-2.amazonaws.com/Screen+Shot+2021-08-19+at+8.34.47+PM.png",
-		info: "9.87 USD",
+		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
 		github: "https://github.com/TravisWoolston",
 		linkedin: 'https://www.linkedin.com/in/traviswoolston/'	  
 	}
