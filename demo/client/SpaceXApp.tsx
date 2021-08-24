b801166e4618330ecac774e609877cfb0b949d3e
Travis Woolston <travis.ww@hotmail.com>
2021-08-23 20:28:55 -0600

    optimized cache route with simple queries, added clearCache middleware

--- demo/client/SpaceXApp.tsx
+++ demo/client/SpaceXApp.tsx
@@ -1,39 +1,36 @@
-import React, { Component} from 'react';
+import React, { Component, useState} from 'react';
 import axios from 'axios';
-import { request, gql } from 'graphql-request'
-import RediQLCache from '../server/RediQLCache/src/RediQL'
-const rediQL = new RediQLCache()
+// import { request, gql } from 'graphql-request'
 
 
 const SpaceXApp = () => {
-   
-    const spaceXQuery = gql`
-{
-  launches {
-    flight_number
-    mission_name
-    launch_date_utc
-    launch_success
-  }
-}`
+    
+const [spaceXData, setSpaceXData] = useState('');
 
-// const spaceXQueryRQL = new RediQLCache(spaceXQuery)
-// RediQLCache.query()
+const request = async () => {
+   const {data} = await axios('http://localhost:1500/rediql')
+   .then(data => data)
+//    console.log('spaceXApp request => ', data.data.launches[0].mission_name)
+console.log(data)
+    setSpaceXData(JSON.stringify(data.launches))
+    // console.log(typeof data)
+   return data
+}
+ 
+const clearCache = () => {
+    axios('http://localhost:1500/clearCache')
+    
 
-// request('/graphql', spaceXQuery).then((res) => {
-//     console.log(res)
-// }).catch(((err) => {
-//     console.log(err)
-// }));
-    // request('/graphql', spaceXQuery).then((res) => {
-    //     console.log(res)
-    // }).catch(((err) => {
-    //     console.log(err)
-    // }));
+}
+// const request = () => console.log('button clicked')
 
         return (
             <div className="SpaceXApp">
-                SpaceX
+  
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"  onClick={() => request()}>SpaceX Button</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"  onClick={() => clearCache()}>Clear Cache</button>
+                
+            <div className="spaceXData"> {spaceXData} </div>
             </div>
         )
     
