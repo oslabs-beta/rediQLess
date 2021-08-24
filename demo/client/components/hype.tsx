3c4337dd6868232c8e783e6c69bc6970f5edbea5
Charlie Malave <malavecharles@gmail.com>
2021-08-24 17:07:56 -0400

    front end overhaul, added hover animations, modals, cards, etc

--- demo/client/components/hype.tsx
+++ demo/client/components/hype.tsx
@@ -5,27 +5,26 @@ import React from "react";
 const Hype = () => {
 	return (
 	<div>
-		<div className="flex justify-evenly py-12">
-			<div className="text-center">
+		<div className="flex justify-evenly py-12 mx-10 ">
+			<div className="transform transition duration-500 hover:scale-110 text-center mx-20 text-white-default bg-darkblue-default shadow-2xl rounded-lg">
 				<h1 className="mb-6">Hype 1</h1>
-                <button className="transition duration-300 ease-in-out py-3 px-6 text-white rounded-lg bg-deeppink-default">LOADING</button>
-				<article>Lorem ipsum blah blah blah</article>
+				<article>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset s</article>
 			</div>
-            <img className="max-w-md transition ease-out duration-700" src="http://neologisms.blogs.wm.edu/files/2016/03/vaporwave-1.jpg"/>
+            <img className="max-w-md transition ease-out duration-700 rounded-lg" src="http://neologisms.blogs.wm.edu/files/2016/03/vaporwave-1.jpg"/>
 		</div>
-		<div className="flex justify-evenly py-12">
-			<img className="max-w-md"  src="http://neologisms.blogs.wm.edu/files/2016/03/vaporwave-1.jpg"/>
-			<div className="text-center">
-				<h1 className="mb-6">Hype 1</h1>
-				<article>Lorem ipsum blah blah blah</article>
+		<div className="flex justify-evenly py-12 mx-10">
+			<img className="max-w-md rounded-lg"  src="http://neologisms.blogs.wm.edu/files/2016/03/vaporwave-1.jpg"/>
+			<div className="transform transition duration-500 hover:scale-110 text-center mx-20 text-white-default bg-darkblue-default shadow-2xl rounded-lg">
+				<h1 className="mb-6">Hype 2</h1>
+				<article>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset s</article>
 			</div>
 		</div>
-		<div className="flex justify-evenly py-12">
-		<div className="text-center">
-				<h1 className="mb-6">Hype 1</h1>
-				<article>Lorem ipsum blah blah blah</article>
+		<div className="flex justify-evenly py-12 mx-10 ">
+			<div className="transform transition duration-500 hover:scale-110 text-center mx-20 text-white-default bg-darkblue-default shadow-2xl rounded-lg">
+				<h1 className="mb-6">Hype 3</h1>
+				<article>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset s</article>
 			</div>
-			<img className="max-w-md transform hover:animate-bounce hover:rotate-45"  src="http://neologisms.blogs.wm.edu/files/2016/03/vaporwave-1.jpg"/>
+            <img className="max-w-md transition ease-out duration-700 rounded-lg" src="http://neologisms.blogs.wm.edu/files/2016/03/vaporwave-1.jpg"/>
 		</div>
 	</div>
 		
