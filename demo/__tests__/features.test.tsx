b5298d7a0c9bf43e09492d33567bdd87617789da
eric saldivar <esaldivar1214@gmail.com>
2021-09-20 22:03:39 -0500

    reacting testing branch initial set up

---
+++ demo/__tests__/features.test.tsx
@@ -0,0 +1,15 @@
+// import dependencies
+import React, {FC, ReactElement} from 'react'
+
+// import react-testing methods
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+
+//import Features Container
+import FeaturesContainer from "../client/containers/featurecontainer"
+
+//import the feature child
+import Features from '../client/components/features'
+
+test('shows the children of Features Container', ()=> {
+	render
+})
