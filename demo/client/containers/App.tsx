e4168953ce437637bbc435466abfa0af4e8b09d0
eric saldivar <esaldivar1214@gmail.com>
2021-09-24 13:16:17 -0500

    animations are successful

--- demo/client/containers/App.tsx
+++ demo/client/containers/App.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react'
+import { useTransition, animated } from 'react-spring'
 import '../index.css'
 import Navbar from './navcontainer'
 import '@fortawesome/fontawesome-free/css/all.min.css'
@@ -26,23 +27,47 @@ export const App: React.FC = () => {
     team: false
   });
 
+  const featuresTransition = useTransition(currentNav.features, {
+    from: {x: -100, y: 800, opacity: 0},
+    enter: {x: 0, y: 0, opacity: 1},
+    leave: {x: 100, y: 800, opacity: 0}
+  });
+
+  const demoTransition = useTransition(currentNav.demo, {
+    from: {x: -100, y: 800, opacity: 0},
+    enter: {x: 0, y: 0, opacity: 1},
+    leave: {x: 100, y: 800, opacity: 0}
+  });
+
+  const teamTransition = useTransition(currentNav.team, {
+    from: {x: -100, y: 800, opacity: 0},
+    enter: {x: 0, y: 0, opacity: 1},
+    leave: {x: 100, y: 800, opacity: 0}
+  });
+
   return (
     <>
       <Navbar currentNavChange={changeCurrentNav}/>
-      {currentNav.features &&
-        <div>
-          <FeaturesContainer />
-          <HypeContainer />
-        </div> 
-      }
-      {currentNav.demo && 
-        <TimeContext.Provider value={{  timeData, setTimeData, changeTimeData }}>
-          <DemoContainer />
-        </TimeContext.Provider>
-      }
-      {currentNav.team && 
-        <AuthorsContainer />
-      }
+      {featuresTransition((style, item) =>
+          item ? <animated.div style={style} className="item">
+                  <FeaturesContainer />
+                  <HypeContainer />
+                </animated.div> : ''
+      )}
+      {demoTransition((style, item) =>
+        item ? <animated.div style={style} className="item">
+                <TimeContext.Provider value={{  timeData, setTimeData, changeTimeData }}>
+                    <DemoContainer />
+                </TimeContext.Provider>
+              </animated.div> : ''
+      )}
+        {teamTransition((style, item) =>
+          item ? <animated.div style={style} className="item">
+                <AuthorsContainer />
+                </animated.div> : ''
+        )}
+      
+    
       
       { /* setting up TimeContext provider  and passing it the values we want to pass into DemoContainer as props (query.tsx utilizes timeData and ChangeTimeData, chart.tsx utilizes timeData to poppulate graph with information) */ } 
     </>
