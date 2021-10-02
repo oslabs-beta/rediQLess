62d239ad423a7e42ff1d31166ae82d5f9bd09cdd
eric saldivar <esaldivar1214@gmail.com>
2021-10-01 21:35:34 -0500

    adding redis port

--- server/RediQLCache/src/RediQL.js
+++ server/RediQLCache/src/RediQL.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 const { request} = require('graphql-request')
 const redis = require('redis')
 const REDIS_PORT = process.env.PORT || 6379
-const redisClient = redis.createClient(REDIS_PORT);
+const redisClient = redis.createClient(process.env.REDIS_PORT);
 const ExpCache = require('./ExperimentalCache')
 
 
