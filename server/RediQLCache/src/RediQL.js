3083a4381a5e9eb1773a3ee644dfbefa437490ec
eric saldivar <esaldivar1214@gmail.com>
2021-10-04 21:10:10 -0500

    post change to rediql route

--- server/RediQLCache/src/RediQL.js
+++ server/RediQLCache/src/RediQL.js
@@ -2,44 +2,39 @@ const { parse } = require('graphql/language/parser')
 const { visit, BREAK } = require('graphql/language/visitor')
 const { graphql } = require('graphql')
 const axios = require('axios')
-const { request} = require('graphql-request')
+const { request } = require('graphql-request')
 const redis = require('redis')
 const ExpCache = require('./ExperimentalCache')
-const REDIS_URL = process.env.REDIS_URL || 6379;
+const REDIS_URL = process.env.REDIS_URL || 6379
 
-
-const redisClient = redis.createClient(REDIS_URL);
-
-
+const redisClient = redis.createClient(REDIS_URL)
 
 class RediQLCache {
   // establish our props
   constructor() {
     // bind our parser function to the constructor function
     this.parser = this.parser.bind(this)
-    // set up a boilerplate query to send to spaceX API 
+    // set up a boilerplate query to send to spaceX API
     // ** THIS WILL BE PHASED OUT, QUERIES WILL BE COMING FROM THE FRONT
     this.QLQuery = ''
-            
 
-     // ESTABLISHES REQUEST FROM GQL QUERY --- DOUBLE CHECK
+    // ESTABLISHES REQUEST FROM GQL QUERY --- DOUBLE CHECK
     this.request = request
 
     // METHOD THAT ACTUALLY PERFORMS THE QUERY TO THE API
     this.query = this.query.bind(this)
- 
+
     // INITIALIZES CACHE IN THE SERVER - ESTBALISHES CONNECTION
     this.redisClient = redisClient
 
     //THIS.RESPONSE WILL RETURN VALUE FROM CACHE, ELSE IT WILL BE ASSIGNED THE RES FROM GQL -> THEN REASSIGNED TO THIS.CACHE()
-    this.response 
+    this.response
 
     // ALLOWS USER TO CLEAR THE REDIS CACHE
     this.clearCache = this.clearCache.bind(this)
 
     // CHECK IF THE DATA IS IN THE CACHE, INIT AS FALSE, IF DATA IS IN CACHE REDIRESPONSE BECOMES TRUE
     this.rediResponse = false
-    
   }
 
   // PARSER METHOD WILL BE CALLED WITH AN ARG OF FALSE IF WE ARE CHECKING IF A RES CAN BE FORMED FROM THE CACHE
@@ -60,61 +55,63 @@ class RediQLCache {
     await expCache.createQuery()
 
     // IF CACHE RESPONSE IS TRUE, CACHE THE RESPONSE IN REDIS
-    // CONTINUE REVIEW FROM HERE** 
-    if(cacheResponse) await expCache.cacheResponse() 
+    // CONTINUE REVIEW FROM HERE**
+    if (cacheResponse) await expCache.cacheResponse()
 
     this.rediResponse = expCache.rediResponse
     console.log('expCache.rediResponse', expCache.rediResponse)
-    if(expCache.rediResponse) {
+    if (expCache.rediResponse) {
       this.response = expCache.newResponse
     }
   }
 
   async query(req, res, next) {
+    console.log('inside query function inside RediQL')
     this.QLQuery = req.body.data.query
+    console.log('this.QLQuery', this.QLQuery)
     // RUN THE PARSER IF THE CACHE RESP IS FALSE, AWAIT FOR IT TO FINISH
     await this.parser(false)
-    
+
+    console.log('ran parser')
+
     //IF REDIRESPONSE IS TRUE, THERE IS DATA IN THE CACHE, SAVE THE RESPONSE ON RES.LOCALS.QUERY
     if (this.rediResponse) {
       res.locals.query = this.response
 
-    // THIS.RESPONSE TEST
-    console.log('this.response test:')
-    for(let i = 0; i < 2; i++) {   
-      console.log(this.response['launches'][i])
-    }
-  
-    // MOVE TO NEXT PIECE OF MW
+      // THIS.RESPONSE TEST
+      console.log('this.response test:')
+      for (let i = 0; i < 2; i++) {
+        console.log(this.response['launches'][i])
+      }
+
+      // MOVE TO NEXT PIECE OF MW
       return next()
-
     } else {
-
       // IF THE DATA IS NOT IN THE CACHE...
       // RESPONSE DATA IS INITIALIZED
-      let responseData;
-        // MAKING A REQUEST TO GQL, ON 1500/GQL, WITH THE QUERY FROM THE FRONT END
-        responseData = await this.request(
-          'http://localhost:1500/graphql',  
-          this.QLQuery
-        )
-        
+      let responseData
+      // MAKING A REQUEST TO GQL, ON 1500/GQL, WITH THE QUERY FROM THE FRONT END
+      responseData = await this.request(
+        'http://localhost:1500/graphql',
+        this.QLQuery
+      )
+
       // SENDING THE RESPONSE DATA FROM GQL TO THE FRONT
       res.locals.query = responseData
 
       // SAVING THE RESPONSE DATA FROM GQL TO THIS.RESPONSE
-      this.response = responseData 
+      this.response = responseData
       console.log('this.response test:')
-      for(let i = 0; i < 2; i++) {   
+      for (let i = 0; i < 2; i++) {
         console.log(this.response['launches'][i])
       }
       // THIS.PARSER USES PARSER METHOD
       // SEND NEW RESPONSE FROM API THROUGH THE PARSER, SO THE DATA GETS CACHED
-      if(!this.rediResponse) await this.parser()
+      if (!this.rediResponse) await this.parser()
 
       //MOVE TO NEXT PIECE OF MW
       next()
-    } 
+    }
   }
 
   // THIS CLEARS THE CACHE
--- server/server.js
+++ server/server.js
@@ -2,21 +2,17 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const path = require('path')
 const app = express()
-const {graphqlHTTP} = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
 const PORT = process.env.PORT || 1500
 const schema = require('./schema/schema')
 const RediQLCache = require('./RediQLCache/src/RediQL')
 const cors = require('cors')
 
 const RediQL = new RediQLCache()
-
 const RediQLQuery = RediQL.query
 const RediQLClear = RediQL.clearCache
 
-
 app.use(cors())
-
-
 
 app.use(express.json())
 
@@ -32,14 +28,25 @@ make sure to access it on the gqlHTTP object
 *
 */
 
-
-
-app.get('/rediql', RediQLQuery, (req, res) => { 
-
-  // console.log('res.locals.query => ', res.locals.query);
-  // console.log('req.body.query =>', req.body.data.query)
-  res.send(res.locals.query) 
-})
+app.post(
+  '/rediql',
+  (req, res, next) => {
+    console.log('hitting rediql endpoint', req.body.data.query)
+    console.log('RediQLQuery', RediQLQuery)
+    next()
+  },
+  // async (req, res, next) => {
+  //   await RediQLQuery(req, res, next)
+  //   next()
+  // },
+  RediQLQuery,
+  (req, res) => {
+    // console.log('res.locals.query => ', res.locals.query);
+    // console.log('req.body.query =>', req.body.data.query)
+    console.log('exiting rediql endpoint', res.locals.query)
+    res.send(res.locals.query)
+  }
+)
 
 app.use('/clearcache', RediQLClear, (req, res) => {
   res.send('cache cleared')
@@ -47,30 +54,24 @@ app.use('/clearcache', RediQLClear, (req, res) => {
 
 app.use(`/graphql`, graphqlHTTP({ schema, graphiql: true }))
 
-
-
-
-
-
 // statically serve everything in the build folder on the route '/build'
-app.use('/build', express.static(path.join(__dirname, '../build')));
+app.use('/build', express.static(path.join(__dirname, '../build')))
 // serve index.html on the route '/'
 app.get('/', (req, res) => {
-  return res.sendFile(path.join(__dirname, '../index.html'));
-});
+  return res.sendFile(path.join(__dirname, '../index.html'))
+})
 // // Catch all unknown routes(404)
-
 
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
     status: 400,
     message: { err: 'An error occurred' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
-});
+  }
+  const errorObj = Object.assign({}, defaultErr, err)
+  console.log(errorObj.log)
+  return res.status(errorObj.status).json(errorObj.message)
+})
 
 //start server
 app.listen(PORT, () => {
