bdfc28422b7a0be8dbf1779afec5a97cc8510cd3
Charlie Malave <malavecharles@gmail.com>
2021-10-05 22:28:33 -0400

    CREATED NPM BRANCH FOR REDIQLESS

--- demo/client/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from 'react-dom'
-import { App } from './containers/App'
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'))
