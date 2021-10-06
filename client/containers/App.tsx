15e0f92934469cab7882585b9c7a6b96cc8e81d9
eric saldivar <esaldivar1214@gmail.com>
2021-10-05 23:08:25 -0500

    button on nav is responsive

--- client/containers/App.tsx
+++ client/containers/App.tsx
@@ -35,7 +35,7 @@ export const App: React.FC = () => {
   const transitionStyle = {
     from: {x: -1000, y: 0, opacity: 0},
     enter: {x: 0, y: 0, opacity: 1 },
-    leave: {x: 1000, y: -0, opacity: 0},
+    leave: {x: 1000, y: 0, opacity: 0},
     trail: 1000
   }
 
