14988c1d06c83ea4f7a76bd9c293732c30b92268
Travis Woolston <travis.ww@hotmail.com>
2021-10-09 11:20:40 -0600

    updated with package

--- demo/server/server.js
+++ demo/server/server.js
@@ -25,7 +25,7 @@ app.use(cors())
 
 app.use('/build', express.static(path.resolve(__dirname, '../build')))
 
-app.get('/', (req, res) =>
+app.get('/', (req, res) => 
   res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
 )
 
