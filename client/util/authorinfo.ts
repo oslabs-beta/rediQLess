72cbffb6e0a0db7482134f77fa9adaeb8791d385
eric saldivar <esaldivar1214@gmail.com>
2021-10-12 20:59:10 -0500

    adding author information

--- client/util/authorinfo.ts
+++ client/util/authorinfo.ts
@@ -6,28 +6,28 @@ const authorData = [
 	{
 		name: "Charlie Malave",
 		image: "https://rediqless.s3.us-east-2.amazonaws.com/1629337049016.jpeg",
-		info: "Musician, father-to-be, lover of life.",
+		info: "Charlie is a full-stack software engineer experienced in building elegant user-facing applications while leveraging multiple tech stacks, leading SCRUM practices, and honoring essential UX/UI design principles. As a musician and creative, Charlie has had his hand in multiple projects that has enabled him to become a jack of all trades while working in multiple industries.",
 		github: "https://github.com/cmalave13",
 		linkedin: 'https://www.linkedin.com/in/charlesmalave/'
 	  },
 	  {
 		name: "Eric Saldivar",
 		image: "https://rediqless.s3.us-east-2.amazonaws.com/profile.jpg",
-		info: `Husband, father, veteran, teacher, Marvel Comics fan.`,
+		info: `Eric is a software engineer experienced in React and Node.js, with a deep interest in front end modularization with great state management and reliable back end architecture. He has a keen interest in test driven development, README generation, UX/UI and cloud integration.  Apart from software development, Eric is a father of twins and a veteran who loves to travel.`,
 		github: "https://github.com/esaldivar",
 		linkedin: 'https://www.linkedin.com/in/esaldivar1214/'
 	  },
 	  {
 		name: "Ian Judd",
 		image: "https://media-exp1.licdn.com/dms/image/C4D03AQEOE2BopNqOmg/profile-displayphoto-shrink_800_800/0/1529432272197?e=1635379200&v=beta&t=CcNs7vSOXoStA_orsa2VazEHGbAwNPJMWmvxvYmQkok",
-		info: "Deejay, cyclist, cactus enthusiast, lover of naps.",
+		info: "Ian is a full-stack software engineer with a passion for building immersive but elegant front-end frameworks with an emphasis on progressive aesthetics and design-concepts. His other passions include deejaying, napping, cycling, and his sprawling cactus collection.",
 		github: "https://github.com/ikjudd",
 		linkedin: 'https://www.linkedin.com/in/iankjudd/'
 	  },
 	  {
 		name: "Travis Woolston",
 		image: "https://media-exp1.licdn.com/dms/image/C4D03AQHYrToMqrX8_Q/profile-displayphoto-shrink_800_800/0/1618435070746?e=1635379200&v=beta&t=DeONQx4zzECVOTDOUFKjiAyFCyup0vPv2YDj863yGuE",
-		info: "Musician, cat owner, float-tank obsessive.",
+		info: "Travis is a full-stack JavaScript engineer with a passion for back-end design. He loves learning new ways to optimize code and hitting that sweet constant time-complexity. In his free-time he enjoys making games in Javascript, floating in sensory deprivation tanks, walks in the rain, and recording music with his guitar and keyboard.",
 		github: "https://github.com/TravisWoolston",
 		linkedin: 'https://www.linkedin.com/in/traviswoolston/'	  
 	}
